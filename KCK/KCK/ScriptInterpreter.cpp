403291c9e366fd38f4e3487a464e2a51cabd4483
Elathorn <kamhli@st.amu.edu.pl>
2015-10-17 16:20:26 +0300

    Ulepszenie ScriptInterpretera

--- KCK/KCK/ScriptInterpreter.cpp
+++ KCK/KCK/ScriptInterpreter.cpp
@@ -23,11 +23,15 @@ string ScriptInterpreter::interpretUserInput(string humanInput)
 
 	if (script->at(order) == "go")
 	{
-		_mechanic->moveObject(_mechanic->findShelf(script->at(adjToShelf)),
-			_mechanic->findMovableObject(script->at(colorOfMovableObject), script->at(sizeOfMovableObject)),
-			lvlOfShelfChar);
+		Shelf* shelf = _mechanic->findShelf(script->at(adjToShelf));
+		if (shelf == NULL) //jeœli nie znaleziono pó³ki
+			return "Wozek nie odnalazl polki."; //todo: losowanie odpowiedzi
+		MovableObject* obj = _mechanic->findMovableObject(script->at(colorOfMovableObject), script->at(sizeOfMovableObject));
+		if (obj == NULL) //jeœli nie znaleziono obiektu
+			return "Wozek nie znalazl przedmiotu do przeniesienia."; //todo: losowanie odpowiedzi
+		if (_mechanic->moveObject(shelf, obj, lvlOfShelfChar))
+			return "Nie udalo sie przeniesc przedmiotu. Najprawdopodobniej pelna polka."; //todo: losowanie odpowiedzi
+		return "Hurra, udalo sie!"; //todo: wczytywanie tekstow
 	}
-
-
-	return "";
+	return "Rozkaz niezrozumialy, wozek nie wie co zrobic."; //todo: losowanie odpowiedzi
 }
